4a29a41e2822870faff520db235f6e0d03ceefbe
FranGit <23448990+fransaan@users.noreply.github.com>
2021-05-21 15:14:08 +0200

    Commit the change

--- Basic/Program.cs
+++ Basic/Program.cs
@@ -7,6 +7,7 @@ namespace Basic
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World, again!");
         }
     }
 }
