dfcb84bdfaa94c6632b42bd7373380ba4a17d112
FranGit <23448990+fransaan@users.noreply.github.com>
2021-05-21 15:36:05 +0200

    Project started with a small Hello World!

---
+++ Basic/BasicExercices.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using CSharpLearning;
+
+namespace Basic
+{
+	public class BasicE
+	{
+		public static void Ex1()
+		{
+			Console.WriteLine("Hi! I'm Fran and this is my miniproject, so I can learn about using VS2019 and CSharp!");
+		}
+	}
+
+}
---
+++ Basic/CSharpLearning.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using Basic;
+
+namespace CSharpLearning
+{
+    class CSMain
+    {
+        static void Main(string[] args)
+        {
+            BasicE.Ex1();
+        }
+    }
+}
